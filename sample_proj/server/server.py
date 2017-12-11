0600763d6d0c00de832b43bebf225dffb4c995cf
jaehoon92 <jaehoon92@gmail.com>
2017-12-11 11:27:22 +0900

    Added os library

--- sample_proj/server/server.py
+++ sample_proj/server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 #from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,4 +17,4 @@ def hello():
 if __name__ == '__main__':
 	# Bind the PORT if defined, otherwise default to 5000.
 	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	app.run(port=port)
