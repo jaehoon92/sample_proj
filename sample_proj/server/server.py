1eb0dd2d6d7cfd9aaf5191db670ccc4e0fb8faf1
jaehoon92 <jaehoon92@gmail.com>
2017-12-11 11:30:17 +0900

    Modified host

--- sample_proj/server/server.py
+++ sample_proj/server/server.py
@@ -17,4 +17,4 @@ def hello():
 if __name__ == '__main__':
 	# Bind the PORT if defined, otherwise default to 5000.
 	port = int(os.environ.get('PORT', 5000))
-	app.run(port=port)
+	app.run(host='0.0.0.0', port=port)
